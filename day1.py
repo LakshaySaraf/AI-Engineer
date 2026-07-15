531309e96a015312a6efb8fdcf9014a6ed1517ab
Lakshay Agarwal <lakshay.lakshayagarwal@gmail.com>
2026-07-15 20:32:09 +0530

    Starting of AI

---
+++ day1.py
@@ -0,0 +1,36 @@
+# name = "Lakshay"
+# age = 23
+# height = 5.9
+# is_engineer = True
+
+# print(name)
+# print(age)
+# print(height)
+# print(is_engineer)
+
+
+
+# full_name = "Lakshay Agarwal"
+# country = "India"
+# dream_job = "AI Engineer"
+# favourite_language = "Python"
+
+a = 2
+b = 3
+print("Addition is : " , a+b)
+print("Subtraction is : " , a-b)
+print("Multiplication is : " , a*b)
+print("Division is : " , a/b)
+print("Floor Division is : " , a//b)
+print("Modulus is : " , a%b)
+print("Power is : " , pow(a,b))
+
+
+
+
+x = 10
+y = "10"
+
+print(x == y)   #True
+print(type(x))  #int
+print(type(y))  #String
