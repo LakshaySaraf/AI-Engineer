ade81184954a033c56a4eb3bf311234bc87a2f9f
Lakshay Agarwal <lakshay.lakshayagarwal@gmail.com>
2026-07-16 11:55:11 +0530

    Python Indexing completed

---
+++ day-2/indexing.py
@@ -0,0 +1,48 @@
+# -------------------
+# ------TASK 1-------
+# -------------------
+name = "Gopal"
+print(name[0])
+print(name[2])
+print(name[-1])
+print(name[-2])
+
+# -------------------
+# ------TASK 2-------
+# -------------------
+print(len(name))
+print(name[-len(name)])
+print(name[len(name)-1])
+
+# -------------------
+# ------TASK 3-------
+# -------------------
+language = "Python"
+print(language[0])
+print(language[1])
+print(language[2])
+print(language[3])
+print(language[4])
+print(language[5])
+
+
+# -------------------
+# ------TASK 4-------
+# -------------------
+word = "Germany"
+print(word[0])      # G
+print(word[-1])     # y
+print(word[3])      # m
+print(word[-4])     # m
+
+
+# -------------------
+# ------TASK 5-------
+# -------------------
+# Use of ord and chr to specify use of Unicode
+print(ord("L") ,"is", chr(76))
+print(ord("A") ,"is", chr(65))
+print(ord("z") ,"is", chr(122))
+print(ord("5") ,"is", chr(53))
+
+
