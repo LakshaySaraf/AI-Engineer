70b1d6c7e0252fa145b6e6c4bc684dd005544d2e
Lakshay Agarwal <lakshay.lakshayagarwal@gmail.com>
2026-07-16 12:11:45 +0530

    Python String Slicing

---
+++ day-2/slicing.py
@@ -0,0 +1,46 @@
+
+# -------------------
+# ------TASK 1-------
+# -------------------
+
+name = "Lakshay"
+print(name[0:4])
+print(name[-3:])
+print(name[:])
+print(name[::-1])
+
+
+# -------------------
+# ------TASK 2-------
+# -------------------
+language = "Artificial Intelligence"
+# Artificial
+print(language[0:10])
+print(language[:10])
+
+# Intelligence
+print(language[11:])
+print(language[-12:])
+
+print(language[0:len(language):2])  
+print(language[0:len(language):3])  
+
+
+# -------------------
+# ------TASK 3-------
+# -------------------
+text = "Python"
+print(text[1:4])    #yth
+print(text[:3])     #Pyt
+print(text[3:])     #hon
+print(text[-4:-1])  #tho
+print(text[::-1])   #nohtyP
+
+
+# -------------------
+# ------TASK 4-------
+# -------------------
+email = "lakshayagarwal@gmail.com"
+print("Username is : ", email[:14])
+print("Domain is : ", email[15:20])
+print("Extension is : ", email[-3:])
