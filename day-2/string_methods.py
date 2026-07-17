dac7e9fbc0e16145aa97b6a8fddfca27b3a7353a
Lakshay Agarwal <lakshay.lakshayagarwal@gmail.com>
2026-07-17 09:48:15 +0530

    String Methods in Python

---
+++ day-2/string_methods.py
@@ -0,0 +1,40 @@
+# -------------------
+# ------TASK 1-------
+# -------------------
+name = "lakshay agarwal"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())
+
+# -------------------
+# ------TASK 2-------
+# -------------------
+text = "     Python AI     "
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
+
+# -------------------
+# ------TASK 3-------
+# -------------------
+city = "london"
+city.upper()
+print(city)
+city = city.upper()
+print(city)
+
+
+
+
+
+# ---------------------------
+# ------MINI CHALLENGE-------
+# ---------------------------
+original = "    lakshay AGARWAL "
+print(original)
+print(original.strip())
+print(original.strip().title())
+print(original.strip().upper())
+print(original.strip().lower())
